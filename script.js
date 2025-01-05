bd92b0932b73b2fa5f5f12fb3df49166e652a611
AbdKay <94052724+AbdKay@users.noreply.github.com>
2025-01-05 20:58:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-// Currently empty. If you'd like to add JavaScript functionality,
-// such as a mobile menu toggle or additional interactivity, you can do so here.
 
-// Example: Simple console message
 console.log("Welcome to Abdullah Kayssoun’s Portfolio!");
