cff3828307af85b3bab76ea698069ee41480d2e7
AbdKay <94052724+AbdKay@users.noreply.github.com>
2025-01-05 20:57:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Currently empty. If you'd like to add JavaScript functionality,
+// such as a mobile menu toggle or additional interactivity, you can do so here.
+
+// Example: Simple console message
+console.log("Welcome to Abdullah Kayssoun’s Portfolio!");
